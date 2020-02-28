b59cf70e130309725ca0bb73c0f0ac235aac38dd
Petar Korudzhiev <petar.korudzhiev@mentormate.com>
2020-02-28 15:06:58 +0200

    Added resource in the stack

--- lib/notifications-service-stack.ts
+++ lib/notifications-service-stack.ts
@@ -1,12 +1,14 @@
 import * as cdk from '@aws-cdk/core';
-import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs'
-import { Runtime } from '@aws-cdk/aws-lambda';
+import * as sns from '@aws-cdk/aws-sns';
 
 export class NotificationsServiceStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
   
 
+      const topic = new sns.Topic(this, 'Topic', {
+        displayName: 'Customer subscription topic'
+      })
 
     }
 } 
